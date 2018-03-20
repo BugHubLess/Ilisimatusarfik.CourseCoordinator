268a83397d7442eb8385849cdd6008b695cda0dc
Johnny <jms@inuplan.gl>
2018-03-20 07:04:30 -0300

    feat: Add action objects, enroll and roll call

---
+++ Ilisimatusarfik.CourseCoordinator.Commons/Ilisimatusarfik.CourseCoordinator.Commons/Actions/IEnroll.cs
@@ -0,0 +1,8 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.Commons.Actions
+{
+    public interface IEnroll
+    {
+        int CourseID { get; set; }
+        int StudentID { get; set; }
+    }
+}
---
+++ Ilisimatusarfik.CourseCoordinator.Commons/Ilisimatusarfik.CourseCoordinator.Commons/Actions/IRollCall.cs
@@ -0,0 +1,9 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.Commons.Actions
+{
+    public interface IRollCall
+    {
+        int StudentID { get; set; }
+        int LectureID { get; set; }
+        bool? IsPresent { get; set; }
+    }
+}
