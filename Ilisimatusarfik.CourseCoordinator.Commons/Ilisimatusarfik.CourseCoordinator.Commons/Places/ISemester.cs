93334894c56b97708108103daac9ea039e8a35b4
Johnny <jms@inuplan.gl>
2018-03-20 21:05:57 -0300

    feat: Add study program and semester types

---
+++ Ilisimatusarfik.CourseCoordinator.Commons/Ilisimatusarfik.CourseCoordinator.Commons/Places/ISemester.cs
@@ -0,0 +1,8 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.Commons.Places
+{
+    public interface ISemester
+    {
+        int Semester { get; set; }
+        ICourse Course { get; set; }
+    }
+}
---
+++ Ilisimatusarfik.CourseCoordinator.Commons/Ilisimatusarfik.CourseCoordinator.Commons/Places/IStudyProgram.cs
@@ -0,0 +1,12 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.Commons.Places
+{
+    using System.Collections.Generic;
+
+    public interface IStudyProgram
+    {
+        int StudyProgramID { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        IList<ISemester> SemesterCourses { get; set; }
+    }
+}
