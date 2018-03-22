8ac1528aafad04c2ba0c72f49b368312de7454bd
Johnny <jms@inuplan.gl>
2018-03-22 13:48:14 -0300

    feat: Add test project with a single test

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Tests/Models/LectureTests/ScheduleConflictMethodTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.Tests.Models.LectureTests
+{
+    using Ilisimatusarfik.CourseCoordinator.Commons.Models.Places;
+    using System;
+    using Xunit;
+
+    public class ScheduleConflictMethodTests
+    {
+        [Fact]
+        public void ShouldHaveNoScheduleConflict()
+        {
+            // Arrange
+            var l1 = new Lecture();
+            var l2 = new Lecture();
+
+            l1.Start = new DateTimeOffset(2018, 3, 22, 10, 0, 0, TimeSpan.FromHours(-3));
+            l1.Duration = TimeSpan.FromHours(2);
+
+            l2.Start = new DateTimeOffset(2018, 3, 22, 12, 0, 0, TimeSpan.FromHours(-3));
+            l2.Duration = TimeSpan.FromHours(1);
+
+            // Act
+            var hasConflict = l1.ScheduleConflict(l2);
+
+            // Assert
+            Assert.False(hasConflict);
+        }
+    }
+}
