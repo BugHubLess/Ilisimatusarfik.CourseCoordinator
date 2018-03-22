821d44894e87d00b00a208e2f9c5bea70f698285
Johnny <jms@inuplan.gl>
2018-03-22 14:36:19 -0300

    feat: Add an ILectureRepository

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Commons/Repositories/ILectureRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.Commons.Repositories
+{
+    using Ilisimatusarfik.CourseCoordinator.Commons.ErrorHandling;
+    using Ilisimatusarfik.CourseCoordinator.Commons.Models.Places;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface ILectureRepository
+    {
+        Task<IList<Lecture>> GetWeekCourseLectures(int courseId, int weekNumber);
+        Task<IList<Lecture>> GetDayCourseLectures(int courseId, DateTimeOffset date);
+        Task<IList<Lecture>> GetCourseLectures(int courseId);
+        Task<Result<Lecture>> CreateCourseLecture(Lecture lecture);
+        Task<Result> UpdateCourseLecture(Lecture lecture);
+        Task<Result> DeleteLecture(int lectureId);
+    }
+}
