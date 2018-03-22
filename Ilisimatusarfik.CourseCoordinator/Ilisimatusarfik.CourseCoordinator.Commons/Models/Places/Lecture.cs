bef263f8d0d58ef708ef7e3c4e9f9e214cdfbc00
Johnny <jms@inuplan.gl>
2018-03-22 13:05:45 -0300

    feat: Add ScheduleConflict comparer in Lectures model

--- Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Commons/Models/Places/Lecture.cs
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Commons/Models/Places/Lecture.cs
@@ -18,5 +18,19 @@
             // the other one, we return -1
             return Start.CompareTo(other.Start);
         }
+
+        public bool ScheduleConflict(Lecture other)
+        {
+            var end = Start.Add(Duration);
+            var otherEnd = other.Start.Add(other.Duration);
+
+            if (other.Start == Start) return true;
+
+            var s1 = (Start < other.Start && other.Start < end);
+            var s2 = (Start < otherEnd && otherEnd < end);
+            var s3 = (other.Start < Start && Start < otherEnd);
+
+            return s1 || s2 || s3;
+        }
     }
 }
