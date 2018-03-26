2baf258a66156477ba7e42b5d6d94b01f25f61e5
Johnny <johkr@uni.gl>
2018-03-26 01:11:08 -0200

    feat: Add test case for conflict method

--- Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Tests/Models/LectureTests/ScheduleConflictMethodTests.cs
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Tests/Models/LectureTests/ScheduleConflictMethodTests.cs
@@ -27,5 +27,27 @@
             // Assert
             Assert.False(hasConflict);
         }
+
+        [Theory]
+        [InlineData(10, 1, 2, 12, 0, 1)]
+        [InlineData(9, 59, 3, 11, 59, 1)]
+        public void WhenOverlapping_ShouldHaveScheduleConflict(int hour1, int minutes1, int duration1, int hour2, int minutes2, int duration2)
+        {
+            // Arrange
+            var l1 = new Lecture();
+            var l2 = new Lecture();
+
+            l1.Start = new DateTimeOffset(2018, 3, 22, hour1, minutes1, 0, TimeSpan.FromHours(-3));
+            l1.Duration = TimeSpan.FromHours(duration1);
+
+            l2.Start = new DateTimeOffset(2018, 3, 22, hour2, minutes2, 0, TimeSpan.FromHours(-3));
+            l2.Duration = TimeSpan.FromHours(duration2);
+
+            // Act
+            var hasConflict = l1.ScheduleConflict(l2);
+
+            // Assert
+            Assert.True(hasConflict);
+        }
     }
 }
