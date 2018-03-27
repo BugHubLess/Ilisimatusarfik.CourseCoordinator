017b409c130a9bd149b6874524ca3215b4df6eff
Johnny <johkr@uni.gl>
2018-03-27 00:27:17 -0200

    feat: Add initial language controller

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.WebAPI/Controllers/LanguageController.cs
@@ -0,0 +1,20 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.WebAPI.Controllers
+{
+    using Ilisimatusarfik.CourseCoordinator.Commons.Repositories;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    public class LanguageController : ApiController
+    {
+        private readonly ILanguageRepository languageRepository;
+
+        public LanguageController(ILanguageRepository languageRepository)
+        {
+            this.languageRepository = languageRepository;
+        }
+    }
+}
