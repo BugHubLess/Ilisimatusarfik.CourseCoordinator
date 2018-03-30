3167edfc6794102b0e212e57e2e9bdcc93cb524d
Johnny <johnnykristianmoebergsoerensen@gmail.com>
2018-03-29 23:35:28 -0200

    feat: Only allow 1 translation per status per language

--- Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/Tables/StatusTranslations.sql
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/Tables/StatusTranslations.sql
@@ -2,5 +2,6 @@
 (
 	[StatusID] INT NOT NULL, 
     [LanguageID] INT NOT NULL, 
-    [Name] NVARCHAR(MAX) NULL 
+    [Name] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_StatusTranslations] PRIMARY KEY ([LanguageID], [StatusID]) 
 )
