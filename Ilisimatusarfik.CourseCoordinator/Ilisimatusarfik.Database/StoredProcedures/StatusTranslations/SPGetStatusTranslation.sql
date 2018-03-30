95a6584b31527fcefd42201774e8bae61d685520
Johnny <johnnykristianmoebergsoerensen@gmail.com>
2018-03-30 02:31:49 -0200

    feat: Add stored procedure get status translations

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/StatusTranslations/SPGetStatusTranslation.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[SPGetStatusTranslation]
+	@statusId int,
+	@languageId int
+AS
+	SELECT StatusID, Name
+	FROM StatusTranslations
+	WHERE StatusID = @statusId AND LanguageID = @languageId
+RETURN 0
