5c7b4037dfe578a280bfcdd2f132b50a004fd3eb
Johnny <johnnykristianmoebergsoerensen@gmail.com>
2018-03-30 01:42:28 -0200

    feat: Add stored procedure add/update status translations

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/StatusTranslations/SPUpdateOrAddStatusTranslation.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[SPUpdateOrAddStatusTranslation]
+	@statusId int,
+	@languageId int,
+	@name nvarchar
+AS
+BEGIN TRANSACTION
+BEGIN TRY
+	UPDATE StatusTranslations
+	SET Name = @name
+	WHERE StatusID = @statusId AND LanguageID = @languageId
+
+	IF @@ROWCOUNT = 0
+	INSERT INTO StatusTranslations (StatusID, LanguageID, Name)
+	VALUES (@statusId, @languageId, @name)
+COMMIT TRANSACTION
+END TRY
+
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+END CATCH
+RETURN 0
