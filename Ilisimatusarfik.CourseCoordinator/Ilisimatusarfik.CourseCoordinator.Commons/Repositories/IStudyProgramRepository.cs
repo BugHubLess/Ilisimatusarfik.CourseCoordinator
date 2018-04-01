30e92b90b77a03cd67312b5f5e4c8bf50e8e5ff1
Johnny <johkr@uni.gl>
2018-03-31 23:57:11 -0200

    feat: Add IStudyProgramRepository class

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Commons/Repositories/IStudyProgramRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.Commons.Repositories
+{
+    using Ilisimatusarfik.CourseCoordinator.Commons.ErrorHandling;
+    using Ilisimatusarfik.CourseCoordinator.Commons.Models.Places;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IStudyProgramRepository
+    {
+        Task<Result<StudyProgram>> CreateStudyProgram(StudyProgram studyProgram, string locale);
+        Task<Result<StudyProgram>> GetStudyProgram(int studyProgramId, string locale);
+        Task<Result<IList<StudyProgram>>> GetAllStudyPrograms(string locale);
+        Task<Result> UpdateOrTranslateStudyProgram(StudyProgram studyProgram, string locale);
+        Task<Result> DeleteStudyProgram(int studyProgramId);
+    }
+}
