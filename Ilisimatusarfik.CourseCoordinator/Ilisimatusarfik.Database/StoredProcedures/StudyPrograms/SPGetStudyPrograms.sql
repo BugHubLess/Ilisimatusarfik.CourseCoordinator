ea0e3361cc19d05b19f34f5289226291468cbd2e
Johnny <johkr@uni.gl>
2018-04-01 00:53:48 -0200

    feat: Add stored procedure for getting all study programs for a specific locale

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/StudyPrograms/SPGetStudyPrograms.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[SPGetStudyPrograms]
+	@locale NVARCHAR(50)
+AS
+	SELECT S.StudyProgramID, SP.Name, SP.Description
+	FROM
+	((SELECT LanguageID, Locale FROM Languages) AS L
+	CROSS JOIN
+	(SELECT StudyProgramID FROM StudyPrograms) AS S)
+	LEFT JOIN StudyProgramsTranslations SP
+	ON
+		S.StudyProgramID = SP.StudyProgramID AND
+		L.LanguageID = SP.LanguageID
+	WHERE L.Locale = @locale
+RETURN 0
