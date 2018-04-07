8afee10a22119b5f057d4cc66d4099096ea89e2b
Johnny <johkr@uni.gl>
2018-04-07 01:14:27 -0200

    feat: Change stored procedure by casting scope ident to int

--- Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/StudyPrograms/SPAddStudyProgram.sql
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/StudyPrograms/SPAddStudyProgram.sql
@@ -7,7 +7,7 @@ BEGIN TRANSACTION
 BEGIN TRY
 	DECLARE @ID INT;
 	INSERT StudyPrograms DEFAULT VALUES
-	SET @ID = SCOPE_IDENTITY();
+	SET @ID = CAST(SCOPE_IDENTITY() AS INT);
 	INSERT INTO StudyProgramsTranslations (StudyProgramID, LanguageID, Name, Description)
 	SELECT @ID, LanguageID, @name, @description FROM Languages WHERE Locale = @locale
 COMMIT TRANSACTION
