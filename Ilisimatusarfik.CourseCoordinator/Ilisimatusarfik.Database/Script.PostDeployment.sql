8d0863d6d786111300053aca3b5e8faa82060b8f
Johnny <johkr@uni.gl>
2018-04-11 20:59:59 -0200

    feat: Add standard languages to DB (gl/da)

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/Script.PostDeployment.sql
@@ -0,0 +1,25 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+IF NOT EXISTS (SELECT * FROM Languages WHERE Locale = 'kl-GL')
+BEGIN
+	EXEC SPAddLanguage
+		@locale = 'kl-GL',
+		@displayname = 'Kalaallisut';
+END
+GO
+IF NOT EXISTS (SELECT * FROM Languages WHERE Locale = 'da-DK')
+BEGIN
+	EXEC SPAddLanguage
+		@locale = 'da-DK',
+		@displayname = 'Dansk';
+END
