09c2138bd1c3ace71fee123c9259df21ca424e28
Johnny <johnnykristianmoebergsoerensen@gmail.com>
2018-05-01 15:31:29 -0200

    docs: Add documentation to GetStudyProgram method

--- Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Commons/Repositories/IStudyProgramRepository.cs
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Commons/Repositories/IStudyProgramRepository.cs
@@ -8,6 +8,14 @@
     public interface IStudyProgramRepository
     {
         Task<Result<StudyProgram>> CreateStudyProgram(StudyProgram studyProgram, string locale);
+
+        /// <summary>
+        /// Get a study program.
+        /// Returns error when not found.
+        /// </summary>
+        /// <param name="studyProgramId">The id of the study program</param>
+        /// <param name="locale">The locale of the content of the study program</param>
+        /// <returns>A result of a study program or a not found error</returns>
         Task<Result<StudyProgram>> GetStudyProgram(int studyProgramId, string locale);
         Task<Result<IList<StudyProgram>>> GetAllStudyPrograms(string locale);
         Task<Result> AddCourseToProgram(int studyProgramId, Course course, int semester, string locale);
