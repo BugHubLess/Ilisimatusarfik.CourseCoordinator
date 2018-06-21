aabacd0811882340507814f396039e003e5b3d12
Johnny <johnnykristianmoebergsoerensen@gmail.com>
2018-06-21 10:36:27 -0200

    feat: Create SP for adding a course to the DB

--- Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/Courses/SPAddCourse.sql
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/Courses/SPAddCourse.sql
@@ -1,6 +1,26 @@
-﻿CREATE PROCEDURE [dbo].[SPAddCourse]
-	@param1 int = 0,
-	@param2 int
+﻿/**
+ Creates a course and returns the ID of the newly created course
+*/
+CREATE PROCEDURE [dbo].[SPAddCourse]
+	@locale NVARCHAR(50),
+	@startDate DateTimeOffset(7),
+	@endDate DateTimeOffset(7),
+	@ECTS INT,
+	@name NVARCHAR(MAX),
+	@description NVARCHAR(MAX)
 AS
-	SELECT @param1, @param2
-RETURN 0
+BEGIN TRANSACTION
+BEGIN TRY
+	DECLARE @ID INT;
+	INSERT INTO Courses (StartDate, EndDate, ECTS)-- This will set SCOPE_IDENTITY
+	VALUES (@startDate, @endDate, @ECTS)
+	SET @ID = SCOPE_IDENTITY();
+	INSERT INTO CourseTranslations(CourseID, LanguageID, Name, Description)
+	SELECT @ID, LanguageID, @name, @description FROM Languages WHERE Locale = @locale
+COMMIT TRANSACTION
+END TRY
+
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+END CATCH
+RETURN @ID
