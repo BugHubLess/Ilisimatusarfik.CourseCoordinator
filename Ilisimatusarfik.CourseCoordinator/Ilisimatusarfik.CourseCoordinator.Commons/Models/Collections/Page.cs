014248216605e471171f10c079bebe19d397bc14
Johnny <johkr@uni.gl>
2018-06-21 05:48:46 -0200

    feat: Setup code structure for SP DB Courses

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Commons/Models/Collections/Page.cs
@@ -0,0 +1,9 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.Commons.Models.Collections
+{
+    public class Page<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public T Items { get; set; }
+    }
+}
