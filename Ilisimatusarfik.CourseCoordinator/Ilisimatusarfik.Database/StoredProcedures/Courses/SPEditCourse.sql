36ce7baffecc28ebbd853a50bcf474b3fbd18dca
Johnny <johnnykristianmoebergsoerensen@gmail.com>
2018-06-21 15:40:03 -0200

    feat: Create SP for editing a course

--- Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/Courses/SPEditCourse.sql
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/Courses/SPEditCourse.sql
@@ -1,6 +1,41 @@
-﻿CREATE PROCEDURE [dbo].[SPEditCourse]
-	@param1 int = 0,
-	@param2 int
+﻿/*
+Edit/update a course
+*/
+CREATE PROCEDURE [dbo].[SPEditCourse]
+	@locale NVARCHAR(50),
+	@startDate DateTimeOffset(7),
+	@endDate DateTimeOffset(7),
+	@ECTS INT,
+	@name NVARCHAR(MAX),
+	@description NVARCHAR(MAX)
 AS
-	SELECT @param1, @param2
-RETURN 0
+BEGIN TRANSACTION
+BEGIN TRY
+	UPDATE C
+
+	SET C.StartDate = @startDate,
+	C.EndDate = @endDate,
+	C.ECTS = @ECTS,
+	C.Name = @name,
+	C.Description = @description
+
+	FROM (
+		SELECT * FROM
+		Courses CS
+		INNER JOIN CourseTranslations CT
+		ON
+		CS.CourseID = CT.CourseID
+		RIGHT JOIN Languages L
+		ON
+		CT.LanguageID = L.LanguageID
+	) AS C
+	
+	WHERE C.Locale = @locale
+
+COMMIT TRANSACTION
+END TRY
+
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+END CATCH
+RETURN @@ROWCOUNT
