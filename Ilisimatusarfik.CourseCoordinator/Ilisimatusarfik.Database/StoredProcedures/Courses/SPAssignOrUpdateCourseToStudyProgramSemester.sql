45fd09ebb09215785c91d9ed95c106e88f8aae7b
Johnny <johnnykristianmoebergsoerensen@gmail.com>
2018-06-21 10:55:04 -0200

    feat: Add SP that assigns a course to a semester

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/Courses/SPAssignOrUpdateCourseToStudyProgramSemester.sql
@@ -0,0 +1,35 @@
+﻿/**
+	Assigns a course to a study program in a semester
+*/
+CREATE PROCEDURE [dbo].[SPAssignOrUpdateCourseToStudyProgramSemester]
+	@courseId INT,
+	@studyProgramId INT,
+	@semester INT
+AS
+BEGIN TRANSACTION
+BEGIN TRY
+	DECLARE @ROWS INT;
+
+	UPDATE StudyProgramCourses
+	SET StudyProgramID = @studyProgramId,
+	CourseID = @courseId,
+	Semester = @semester
+
+	WHERE StudyProgramID = @studyProgramId
+	AND CourseID = @courseId
+
+	SET @ROWS = @@ROWCOUNT
+
+	IF @ROWS = 0
+	BEGIN
+		INSERT INTO StudyProgramCourses (StudyProgramID, CourseID, Semester)
+		VALUES (@studyProgramId, @courseId, @semester)
+		SET @ROWS = @@ROWCOUNT;
+	END
+COMMIT TRANSACTION
+END TRY
+
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+END CATCH
+RETURN @ROWS  -- returns the number of rows affected
