13e62cdabbb81e73784b71993cda848997d5d66c
Johnny <johnnykristianmoebergsoerensen@gmail.com>
2018-07-10 04:14:56 -0200

    feat: Add action request class

---
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.CourseCoordinator.Commons/Actions/RequestClassroom.cs
@@ -0,0 +1,13 @@
+﻿namespace Ilisimatusarfik.CourseCoordinator.Commons.Actions
+{
+    using Ilisimatusarfik.CourseCoordinator.Commons.Models.Users;
+    using System;
+
+    public class RequestClassroom
+    {
+        public int LectureID { get; set; }
+        public int ClassroomID { get; set; }
+        public Employee Requestor { get; set; }
+        public DateTimeOffset RequestedBookingDate { get; set; }
+    }
+}
