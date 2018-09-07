eb03b6d5150476a59df6d33be73743a951d78674
Johnny <johkr@uni.gl>
2018-09-07 18:30:32 -0200

    feat: SP returns zero when no rows are created

--- Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/LectureStatusTranslations/SPCreateLectureStatusTranslation.sql
+++ Ilisimatusarfik.CourseCoordinator/Ilisimatusarfik.Database/StoredProcedures/LectureStatusTranslations/SPCreateLectureStatusTranslation.sql
@@ -5,11 +5,11 @@ AS
 BEGIN TRANSACTION
 BEGIN TRY
 	DECLARE @ID INT;
-	INSERT LectureStatus DEFAULT VALUES -- This will set SCOPE_IDENTITY
-	SET @ID = SCOPE_IDENTITY();
-	INSERT INTO LectureStatusTranslations (LectureStatusID, LanguageID, Status)
-	SELECT @ID, LanguageID, @status FROM Languages WHERE Locale = @locale
-COMMIT TRANSACTION
+		INSERT LectureStatus DEFAULT VALUES -- This will set SCOPE_IDENTITY
+		INSERT INTO LectureStatusTranslations (LectureStatusID, LanguageID, Status)
+		SELECT @ID, LanguageID, @status FROM Languages WHERE Locale = @locale
+		SET @ID = SCOPE_IDENTITY();
+		COMMIT TRANSACTION
 END TRY
 
 BEGIN CATCH
